b083363d458b41ab143b932d3df52d68f98868c0
Jordan Knox <Jordanknox100@berkeley.edu>
2020-07-07 00:57:37 -0700

    made basic modal implementation for all icons

--- js/script.js
+++ js/script.js
@@ -79,14 +79,35 @@ function setTranslate(xPos, yPos, el) {
 // Get the modal
 var aboutMeModal = document.getElementById("aboutMe-modal");
 var blogModal = document.getElementById("blog-modal");
+var contactMeModal = document.getElementById("contactMe-modal");
+var filmModal = document.getElementById("film-modal");
+var resumeModal = document.getElementById("resume-modal");
+var projectsModal = document.getElementById("projects-modal");
+var photosModal = document.getElementById("photos-modal");
+var referencesModal = document.getElementById("references-modal");
+var linksModal = document.getElementById("links-modal");
 
 // Get the button that opens the modal
 var aboutMebtn = document.getElementById("aboutMe-button");
 var blogbtn = document.getElementById("blog-button");
+var contactMebtn = document.getElementById("contactMe-button");
+var filmbtn = document.getElementById("film-button");
+var resumebtn = document.getElementById("resume-button");
+var projectsbtn = document.getElementById("projects-button");
+var photosbtn = document.getElementById("photos-button");
+var referencesbtn = document.getElementById("references-button");
+var linksbtn = document.getElementById("links-button");
 
 // Get the <span> element that closes the modal
 var aboutMeSpan = document.getElementsByClassName("aboutMeClose")[0];
 var blogSpan = document.getElementsByClassName("blogClose")[0];
+var contactMeSpan = document.getElementsByClassName("contactMeClose")[0];
+var filmSpan = document.getElementsByClassName("filmClose")[0];
+var resumeSpan = document.getElementsByClassName("resumeClose")[0];
+var projectsSpan = document.getElementsByClassName("projectsClose")[0];
+var photosSpan = document.getElementsByClassName("photosClose")[0];
+var referencesSpan = document.getElementsByClassName("referencesClose")[0];
+var linksSpan = document.getElementsByClassName("linksClose")[0];
 
 // When the user clicks on the button, open the modal
 aboutMebtn.ondblclick = function () {
@@ -95,6 +116,28 @@ aboutMebtn.ondblclick = function () {
 blogbtn.ondblclick = function () {
     blogModal.style.display = "block";
 }
+contactMebtn.ondblclick = function () {
+    contactMeModal.style.display = "block";
+}
+filmbtn.ondblclick = function () {
+    filmModal.style.display = "block";
+}
+resumebtn.ondblclick = function () {
+    resumeModal.style.display = "block";
+}
+projectsbtn.ondblclick = function () {
+    projectsModal.style.display = "block";
+}
+photosbtn.ondblclick = function () {
+    photosModal.style.display = "block";
+}
+referencesbtn.ondblclick = function () {
+    referencesModal.style.display = "block";
+}
+linksbtn.ondblclick = function () {
+    linksModal.style.display = "block";
+}
+
 // When the user clicks on <span> (x), close the modal
 aboutMeSpan.onclick = function () {
     aboutMeModal.style.display = "none";
@@ -102,6 +145,28 @@ aboutMeSpan.onclick = function () {
 blogSpan.onclick = function () {
     blogModal.style.display = "none";
 }
+contactMeSpan.onclick = function () {
+    contactMeModal.style.display = "none";
+}
+filmSpan.onclick = function () {
+    filmModal.style.display = "none";
+}
+resumeSpan.onclick = function () {
+    resumeModal.style.display = "none";
+}
+projectsSpan.onclick = function () {
+    projectsModal.style.display = "none";
+}
+photosSpan.onclick = function () {
+    photosModal.style.display = "none";
+}
+referencesSpan.onclick = function () {
+    referencesModal.style.display = "none";
+}
+linksSpan.onclick = function () {
+    linksModal.style.display = "none";
+}
+
 // When the user clicks anywhere outside of the modal, close it
 // window.onclick = function (event) {
 //     if (event.target == modal) {
