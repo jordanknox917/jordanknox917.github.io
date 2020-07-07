0198d2e5b49085ea17e0dc861954a028934bcd09
Jordan Knox <Jordanknox100@berkeley.edu>
2020-07-07 00:04:19 -0700

    added the very basic modal for aboutMe

--- js/script.js
+++ js/script.js
@@ -73,3 +73,31 @@ function drag(e) {
 function setTranslate(xPos, yPos, el) {
     el.style.transform = "translate3d(" + xPos + "px, " + yPos + "px, 0)";
 }
+
+///     Modal Javascript    ///
+
+// Get the modal
+var modal = document.getElementById("aboutMe-modal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("aboutMe-button");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function () {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
